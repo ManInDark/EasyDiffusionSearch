6997dd40b3d39cb2100686a9bc516d622bcc1d28
ManInDark <61268856+ManInDark@users.noreply.github.com>
2023-07-09 23:59:46 +0200

    Fixed problem that image paths didn't actually lead to image

--- ingest.py
+++ ingest.py
@@ -1,7 +1,6 @@
 import sqlite3
 import os
 import asyncio
-import time
 
 IMAGE_ROOT_PATH = "../images"
 
@@ -9,11 +8,11 @@ connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
 if ("image",) not in cursor.execute("SELECT name FROM sqlite_master"):
-    cursor.execute("CREATE TABLE image(path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora)")
+    cursor.execute("CREATE TABLE image(path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora, upscaling, face_correction)")
     print("Created Table, as it did not exist yet")
 
-def insert_image(path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora):
-    cursor.execute("INSERT INTO image VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora))
+def insert_image(path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora, upscaling, face_correction):
+    cursor.execute("INSERT INTO image VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (path, prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora, upscaling, face_correction))
     connection.commit()
 
 def get_value_for(content_string: str, name: str) -> str:
@@ -48,17 +47,41 @@ def parse_lines(content):
     sampler = get_value_for(content_string, "Sampler: ")
     steps = get_value_for(content_string, "Steps: ")
     guidance_scale = get_value_for(content_string, "Guidance Scale: ")
-    lora = get_model_LoRA(content_string) 
+    lora = get_model_LoRA(content_string)
+    upscaling = get_value_for(content_string, "Use Upscaling: ")
+    face_correction = get_value_for(content_string, "Use Face Correction: ")
 
-    return (prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora)
+    return (prompt, negative_prompt, seed, model, width, height, sampler, steps, guidance_scale, lora, upscaling, face_correction)
+
+def test_extensions(path: str) -> tuple[bool, str]:
+    if os.path.isfile(path.replace(".extension", ".jpeg")):
+        return (True, path.replace(".extension", ".jpeg"))
+    elif os.path.isfile(path.replace(".extension", ".png")):
+        return (True, path.replace(".extension", ".png"))
+    else:
+        return (False, path)
 
 async def parse_file_async(folder, txtfile):
-    print(f"Started at {time.strftime('%X')}")
     with open(os.path.join(IMAGE_ROOT_PATH, folder, txtfile), "r") as file:
         lines = file.readlines()
         try:
+            # figure out image path
             parsed_content = parse_lines(lines)
-            insert_image(os.path.join(IMAGE_ROOT_PATH, folder, txtfile.replace(".txt", ".png")), *parsed_content)
+            img_path = os.path.join(IMAGE_ROOT_PATH, folder, txtfile.replace(".txt", ".extension"))
+            res = test_extensions(img_path)
+            if not res[0]:
+                if not parsed_content[11] == "None": # face correction
+                    img_path = img_path.replace(".extension", f"_{parsed_content[11]}.extension")
+                if not parsed_content[10] == "None": # upscaling
+                    img_path = img_path.replace(".extension", f"_{parsed_content[10]}.extension")
+            res = test_extensions(img_path)
+            if not res[0]: 
+                print("Could not find file")
+                print(parsed_content, img_path)
+                exit()
+
+            # insert into database
+            insert_image(img_path, *parsed_content)
         except Exception as e:
             print(lines)
             print(os.path.join(IMAGE_ROOT_PATH, folder, txtfile))
